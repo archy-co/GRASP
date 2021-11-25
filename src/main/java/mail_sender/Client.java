21830ef0ea9d4e6549990951f1e6e14c532ea019
Archy <archy.revera@protonmail.com>
2021-11-25 17:57:32 +0200

    Update client

--- src/main/java/mail_sender/Client.java
+++ src/main/java/mail_sender/Client.java
@@ -1,19 +1,21 @@
 package mail_sender;
 
 public class Client {
-    public static int getNextId() { return nextId; }
+
+    public int getNextId() { return nextId; }
     public String getName() { return name; }
     public int getAge() { return age; }
-    public String getSex() { return sex; }
+    public Sex getSex() { return sex; }
     public int getId() { return id; }
 
     private static int nextId = 0;
     private String name;
     private int age;
-    private String sex;
+    private Sex sex;
 
     private int id;
-    Client(String name_, int age_, String sex_){
+
+    Client(String name_, int age_, Sex sex_){
         id = nextId;
         name = name_;
         age = age_;
